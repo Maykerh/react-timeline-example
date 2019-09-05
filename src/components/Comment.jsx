e7c59d9b705e53e8edce4f88c649d607b2b5d2ee
Mayke <make_zs@hotmail.com>
2019-09-05 19:31:47 -0300

    implementado visualização de comentario

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
-import './styles/Post.css';
+import './styles/Comment.css';
 
-class Comment extends Component {
-    render() {
-        return <div className="Comment-container">asdas</div>;
-    }
-}
-
-export default Comment;
+export default props => {
+    return (
+        <div className="comment-container">
+            <div className="comment-image">
+                <img src={props.data.image} width={35} height={35} />
+            </div>
+            <div className="comment-text-box">
+                <div className="comment-text">
+                    <span className="comment-username">{props.data.user}</span>
+                    {props.data.text}
+                </div>
+            </div>
+        </div>
+    );
+};
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -19,9 +19,14 @@ const data = [
             {
                 user: 'Kevin Bacon',
                 image: kevin,
-                user: 'Zé',
                 text:
                     'No entanto, não podemos esquecer que o entendimento das metas propostas prepara-nos para enfrentar situações atípicas decorrentes dos procedimentos normalmente adotados'
+            },
+            {
+                user: 'Jim Carrey',
+                image: jim,
+                text:
+                    'Podemos já vislumbrar o modo pelo qual a constante divulgação das informações estende o alcance e a importância de todos os recursos funcionais envolvidos. '
             }
         ]
     },
@@ -52,6 +57,18 @@ const data = [
                 image: myles,
                 text:
                     'Pensando mais a longo prazo, o início da atividade geral de formação de atitudes nos obriga à análise do retorno esperado a longo prazo. '
+            },
+            {
+                user: 'Kevin Bacon',
+                image: kevin,
+                text:
+                    'O que temos que ter sempre em mente é que o desenvolvimento contínuo de distintas formas de atuação faz parte de um processo de gerenciamento dos procedimentos normalmente adotados. Desta maneira, a mobilidade dos capitais internacionais afeta positivamente a correta previsão do investimento em reciclagem técnica. Assim mesmo, a hegemonia do ambiente político desafia a capacidade de equalização dos métodos utilizados na avaliação de resultados.'
+            },
+            {
+                user: 'Myles Kennedy',
+                image: myles,
+                text:
+                    'Neste sentido, a percepção das dificuldades auxilia a preparação e a composição do sistema de participação geral. Por outro lado, a expansão dos mercados mundiais não pode mais se dissociar das diversas correntes de pensamento. Podemos já vislumbrar o modo pelo qual a estrutura atual da organização maximiza as possibilidades por conta das novas proposições. O incentivo ao avanço tecnológico, assim como o surgimento do comércio virtual promove a alavancagem de alternativas às soluções ortodoxas.'
             }
         ]
     }
